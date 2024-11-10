f77c3cbd41a5afb5fc2f4b0b0160fe13b1acb776
shved2004 <plamyaonelove@gmail.com>
2024-11-10 13:33:34 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/task1.py
@@ -0,0 +1,14 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 1
+while money_capital >= spend:
+    money_capital += salary
+    money_capital -= spend
+    spend *= (1 + increase)
+    months += 1
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
